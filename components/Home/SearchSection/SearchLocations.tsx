fd7404d04a54f923990dcb12204bbe478d2db568
Muhammed <antar.muhammed1@gmail.com>
2025-02-18 11:46:13 +0300

    refactor: remove unused Text import in SearchLocations component

--- components/Home/SearchSection/SearchLocations.tsx
+++ components/Home/SearchSection/SearchLocations.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { useSearchBar } from "@/context/searchBarContext";
 import FetchWeather from "@/hooks/useFetchWeather";
 import { useWeather } from "@/context/weatherContext";
